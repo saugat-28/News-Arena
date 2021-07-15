00ae33b7710ff0991a9bc7459779314975419523
Saugat <saugat.shrivastava14@gmail.com>
2021-07-15 23:53:52 +0530

    Updated News Arena app and implemented API call to fetch News. Custom Chrome Tabs are used to open news in in-app browser. Layout changed to show thumbnail of the news and author name.

--- app/src/main/java/com/app/newsarena/MainActivity.kt
+++ app/src/main/java/com/app/newsarena/MainActivity.kt
@@ -1,34 +1,64 @@
 package com.app.newsarena
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.app.newsarena.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), NewsItemClicked {
+class MainActivity : AppCompatActivity(), NewsItemClicked{
+
+    private  lateinit var mAdapter: NewsListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        val items = fetchData()
-        binding.recyclerView.adapter = NewsListAdapter(items, this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        binding.recyclerView.adapter = mAdapter
 
     }
 
-    private fun fetchData(): ArrayList<String> {
-        val list = ArrayList<String>()
-        for (i in 0 until 100){
-            list.add("Item $i")
-        }
-        return list
+    private fun fetchData(){
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+                mAdapter.updateNews(newsArray)
+            },
+            {
+                Toast.makeText(this, "Error Fetching Data", Toast.LENGTH_LONG).show()
+            }
+        )
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this, "Item clicked is $item", Toast.LENGTH_LONG).show()
+    override fun onItemClicked(item: News) {
+        val builder = CustomTabsIntent.Builder();
+        val customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(item.url));
     }
 
 }
--- app/src/main/java/com/app/newsarena/NewsListAdapter.kt
+++ app/src/main/java/com/app/newsarena/NewsListAdapter.kt
@@ -3,10 +3,13 @@ package com.app.newsarena
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class NewsListAdapter(private val items: ArrayList<String>,  private val listener: NewsItemClicked) : RecyclerView.Adapter<NewsViewHolder>(){
+class NewsListAdapter(private val listener: NewsItemClicked) : RecyclerView.Adapter<NewsViewHolder>(){
+    private val items: ArrayList<News> = ArrayList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
         val viewHolder = NewsViewHolder(view)
@@ -17,19 +20,28 @@ class NewsListAdapter(private val items: ArrayList<String>,  private val listene
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        holder.titleView.text = items[position]
+        holder.titleView.text = items[position].title
+        holder.author.text = items[position].author
+        Glide.with(holder.itemView.context).load(items[position].imageUrl).into(holder.image)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
+    fun updateNews(updatedItems: ArrayList<News>){
+        items.clear()
+        items.addAll(updatedItems)
 
+        notifyDataSetChanged()
+    }
 }
 
 class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
     val titleView: TextView = itemView.findViewById(R.id.textView)
+    val image: ImageView = itemView.findViewById(R.id.image)
+    val author: TextView = itemView.findViewById(R.id.author)
 }
 
 interface NewsItemClicked{
-    fun onItemClicked(item: String)
+    fun onItemClicked(item: News)
 }
